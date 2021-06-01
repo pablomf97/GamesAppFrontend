c1978843d6a17b5f171fe1d0c61a8d0c5320301a
Pablo Figueroa <pablomf@hotmail.es>
2021-06-01 10:24:37 +0200

    Initial Commit

---
+++ VGSmasher/VGSmasherApp.swift
@@ -0,0 +1,17 @@
+//
+//  VGSmasherApp.swift
+//  VGSmasher
+//
+//  Created by Pablo Figueroa Martínez on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct VGSmasherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
